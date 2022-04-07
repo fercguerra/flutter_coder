7dc012e6e0ab4215f1a91d54fe002cd39bcc8bb9
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-04-07 19:31:59 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import './resultado.dart';
 import './questionario.dart';
 
-main() => runApp(PerguntaApp());
+import 'package:flutter/material.dart';
+import './questionario.dart';
+import './resultado.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
 
 class PerguntaApp extends StatefulWidget {
   const PerguntaApp({Key? key}) : super(key: key);
@@ -12,76 +18,73 @@ class PerguntaApp extends StatefulWidget {
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaSelecionada = 0; 
-  //moveu para State a própria variável
+  var _perguntaSelecionada = 0;
   var _pontuacaoTotal = 0;
   final _perguntas = const [
-      {
-        'texto': 'Qual sua cor favorita?',
-        'respostas': [
-          {'texto': 'Preto', 'nota': 10 }, 
-          {'texto': 'Vermelho', 'nota': 5},
-          {'texto': 'Verde', 'nota': 3}, 
-          {'texto': 'Branco', 'nota': 1},
-        ],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': [
-          {'texto': 'Coelho', 'nota': 10}, 
-          {'texto': 'Cobra', 'nota': 5 }, 
-          {'texto': 'Elefante', 'nota': 3},
-          {'texto': 'Leão', 'nota': 1},
-      ]},
-      {
-        'texto': 'Qual é o seu instrutor favorito?',
-        'respostas': [
-          {'texto': 'Maria', 'nota': 10 },
-          {'texto':'João', 'nota': 5 },
-          {'texto':'Leo','nota': 3},
-          {'texto':'Pedro', 'nota': 1 },
-          ],
-      }
-    ];
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'João', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
+    }
+  ];
+
   void _responder(int pontuacao) {
-    //moveu o método
-    if(temPerguntaSelecionada){
+    if (temPerguntaSelecionada) {
       setState(() {
-      _perguntaSelecionada++;
-      _pontuacaoTotal += pontuacao;
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
     });
   }
-}
-void _reiniciarQuestionario() {
-  setState(() {
-    _perguntaSelecionada = 0;
-    _pontuacaoTotal = 0;
-     print(_pontuacaoTotal);
-  });
-}
 
-bool get temPerguntaSelecionada {
-  return _perguntaSelecionada < _perguntas.length;
- 
-}
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: temPerguntaSelecionada?? false
-        ? Questionario(
-          perguntas: _perguntas, 
-          perguntaSelecionada: _perguntaSelecionada, 
-          responder: _responder,
-          )              
-        : Resultado(_pontuacaoTotal,  _reiniciarQuestionario),
-   
-      )
-    );       
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
+              )
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
+      ),
+    );
   }
 }
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -2,20 +2,20 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-
   final int pontuacao;
-  final void Function() quandoReiniciarQuestionario; 
+  final void Function() quandoReiniciarQuestionario;
 
-  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+  const Resultado(this.pontuacao, this.quandoReiniciarQuestionario, {Key? key})
+      : super(key: key);
 
   String get fraseResultado {
-    if(pontuacao <8 ){
-      return 'Parabéns';
+    if (pontuacao < 8) {
+      return 'Parabéns!';
     } else if (pontuacao < 12) {
       return 'Você é bom!';
-    } else if (pontuacao < 16){
-      return 'Impressionante';
-    } else{
+    } else if (pontuacao < 16) {
+      return 'Impressionante!';
+    } else {
       return 'Nível Jedi!';
     }
   }
@@ -23,31 +23,25 @@ class Resultado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('Parabéns!!!'),
+        Center(
+          child: Text(
+            fraseResultado,
+            style: const TextStyle(fontSize: 28),
+          ),
+        ),
         TextButton(
-          onPressed: (){}, 
-          child: Text(''),),
+          child: const Text(
+            'Reiniciar?',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.blue,
+            ),
+          ),
+          onPressed: quandoReiniciarQuestionario,
+        )
       ],
-
     );
-    
   }
-
 }
-
-// Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: <Widget> [
-//         Text(
-//           'Parabéns !',
-//               style: TextStyle(fontSize: 28 ),
-//               ),
-//               ElevatedButton(
-//                 child: Text('Reiniciar?',
-//                 style: TextStyle(fontSize: 18),
-//                 textColor: Colors.blue,
-//                 onPressed: quandoReiniciarQuestionario,
-//               ),
-//       ],     
-//     );
